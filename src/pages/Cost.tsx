907b60ad51a1efef153c44fcd9166d81306e00ae
Tianrider <hadiwijayachristian7@gmail.com>
2024-07-28 06:58:40 +0700

    refactor: Improve Cost page layout and add summary table

--- src/pages/Cost.tsx
+++ src/pages/Cost.tsx
@@ -8,7 +8,7 @@ export default function Cost() {
                 <ArrowLeft className="h-10 w-10 scale-90 text-white" />
             </div>
             <div className="flex flex-col gap-4">
-                <div className="mt-10 text-white">
+                <div className="text-white">
                     <p>
                         We Estimated that the repair <br /> will cost you:{" "}
                     </p>
@@ -17,8 +17,38 @@ export default function Cost() {
                     Rp 100.000 - Rp 500.000
                 </div>
             </div>
-            <div className="rounded-[10px] h-2/3 overflow-y-scroll bg-white shadow-2xl"></div>
-            <button className="w-full rounded-full bg-green-600 py-3 text-center font-bold text-white">
+            <div className="rounded-[10px] flex h-2/3 flex-col items-start overflow-y-scroll bg-white p-4 shadow-2xl">
+                <p className="font-bold">Summary</p>
+                <table className="w-full text-sm">
+                    <thead>
+                        <tr>
+                            <th className="w-1/2"></th>
+                            <th className="w-1/4">Minimum</th>
+                            <th className="w-1/4">Maximum</th>
+                        </tr>
+                    </thead>
+                    <tbody className="text-xs">
+                        <tr className="h-7">
+                            <td>Service Fee</td>
+                            <td className="text-center">Rp 50.000</td>
+                            <td className="text-center">Rp 100.000</td>
+                        </tr>
+                        <tr className="h-7">
+                            <td>Parts Fee</td>
+                            <td className="text-center">Rp 50.000</td>
+                            <td className="text-center">Rp 100.000</td>
+                        </tr>
+                    </tbody>
+                    <tfoot className="mt-4 h-10 border-t-2 text-xs">
+                        <tr className="h-7">
+                            <td></td>
+                            <td className="text-center">Rp 100.000</td>
+                            <td className="text-center">Rp 200.000</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+            <button className="mt-2 w-full rounded-full bg-green-600 py-3 text-center font-bold text-white">
                 Back Home
             </button>
         </div>
